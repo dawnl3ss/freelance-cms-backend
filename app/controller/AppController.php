164414f43719d851ac2d43bdc8f36f8964467ead
.dawnless <52249849+dawnl3ss@users.noreply.github.com>
2025-11-21 15:55:07 +0100

    update [0.2.1] : DatabaseWrapper class enhanced

--- app/controller/AppController.php
+++ app/controller/AppController.php
@@ -5,7 +5,6 @@ namespace app\controller;
 use api\format\JsonResponse;
 
 use modules\database\DatabaseWrapper;
-use modules\database\drivers\DatabaseDriverEnum;
 
 class AppController {
 
@@ -14,16 +13,6 @@ class AppController {
      * [@route] => /
      */
     public function index(){
-        $wrapper = DatabaseWrapper::_getDriver()->_database("hardware_hub")->_query("SHOW DATABASES;");
-        var_dump($wrapper);
         echo "Homepage demo for automated Router/Controller";
-    }
-
-    /**
-     * [@method] => GET
-     * [@route] => /caca
-     */
-    public function cacaccaccaca(){
-        print_r((new JsonResponse())->_add("test", "vam bab"));
     }
 }
--- modules/database/DatabaseWrapper.php
+++ modules/database/DatabaseWrapper.php
@@ -9,17 +9,84 @@ use modules\database\drivers\DatabaseDriverEnum;
 use modules\database\drivers\list\DatabasePdoDriver;
 
 
-class DatabaseWrapper implements DatabaseConnectable {
+class DatabaseWrapper {
+
+    /** @var string $_database */
+    private string $_database;
+
+    /** @var DatabaseDriver $_driver */
+    private DatabaseDriver $_driver;
+    
+    public function __construct(string $database){
+        $this->_driver = $this->_getDriver();
+        $this->_database = $database;
+    }
+
+
+    /**
+     * Operate a SQL 'SELECT' query
+     *
+     * @param string $table
+     * @param string $content
+     * @param array $assoc
+     *
+     * @return mixed
+     */
+    public function _select(string $table, string $content, array $assoc = []){
+        $query = "SELECT {$content} FROM {$table}";
+
+        if (count(array_keys($assoc)) > 1){
+            $query .= " WHERE ";
+            $i = 1;
+
+            foreach (array_keys($assoc) as $key){
+                $query .= "{$key} = :{$key}";
+                if ($i > 1) $query .= " AND ";
+                $i++;
+            }
+        }
+
+        return $this->_driver->_database($this->_database)->_query($query, $assoc);
+    }
+
+
+    /**
+     * Operate a 'INSERT INTO' SQL query
+     *
+     * @param string $table
+     * @param array $assoc
+     *
+     * @return mixed
+     */
+    public function _insert(string $table, array $assoc){
+        $query = "INSERT INTO {$table} (" . implode(',', array_keys($assoc)) . ") VALUES (:" . implode(',:', array_keys($assoc)) . ")";
+        return $this->_driver->_database($this->_database)->_query($query, $assoc);
+    }
+
+
+    /**
+     * Check if a value is in a table
+     *
+     * @param $table
+     * @param string $content
+     * @param array $assoc
+     *
+     * @return bool
+     */
+    public function _exist($table, string $content, array $assoc = []){
+        return $this->_select($table, $content, $assoc) != [];
+    }
 
 
     /**
      * @return DatabaseDriver
      */
-    public static function _getDriver() : DatabaseDriver {
+    private function _getDriver() : DatabaseDriver {
         /*return match ($driverEnum){
             DatabaseDriverEnum::PDO => new DatabasePdoDriver(),
             default => new DatabasePdoDriver(),
         };*/
         return new DatabasePdoDriver();
     }
+
 }
